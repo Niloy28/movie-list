4e628dc9a4b8cddb2aa2f7a54c2c6bdfc9915af2
Farhan Ishrak Islam <niloy28@live.com>
2023-03-16 01:56:03 +0600

    Handle fetch in useEffect.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 
 import "./App.css";
@@ -8,7 +8,7 @@ function App() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	const fetchMovieList = async () => {
+	const fetchMovieList = useCallback(async () => {
 		setError(null);
 		setIsLoading(true);
 
@@ -35,7 +35,11 @@ function App() {
 		}
 
 		setIsLoading(false);
-	};
+	}, []);
+
+	useEffect(() => {
+		fetchMovieList();
+	}, [fetchMovieList]);
 
 	let content = <p>No movies found.</p>;
 	if (!isLoading && movies.length > 0) {
